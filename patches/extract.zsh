aa49b1266f56b228987e3815c5019b1c255975ee
Laurent Georget <laurent.georget@supelec.fr>
2016-04-25 11:01:42 +0200

    Add scripts to manage patches

---
+++ patches/extract.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+curdir=$(pwd)
+repo=/home/lgeorget/projects/kayrebt/kayrebt-path_examiner/linux
+base_commit=bbb70443
+
+pushd $repo
+while read -A line; do
+	syscall=${line[1]}
+	branch=${line[2]}
+	commit=${line[3]}
+	patch_file=$curdir/$syscall.patch
+	git checkout $branch
+	git diff -p $base_commit $commit -- . > $patch_file
+done
+popd
