f9b2591a79c3efd1131364568dd91aa2907d9786
Laurent Georget <laurent.georget@supelec.fr>
2016-04-25 17:40:19 +0200

    Publish the results

--- patches/producer.zsh
+++ patches/producer.zsh
@@ -7,8 +7,10 @@ while read -A line; do
 	func=${line[2]}
 	file=${line[3]}
 
-	patch -f -p2 < patches/$syscall.patch
+	git checkout -f $syscall
+#	patch -f -p2 < patches/$syscall.patch
 	rm ${file/%.c/.o}
 	make CC=gcc-4.8.5 HOSTCC=gcc-4.8.5 EXTRA_CFLAGS="-fdump-tree-optimized -fplugin=kayrebt_pathexaminer2 -fplugin-arg-kayrebt_pathexaminer2-function=$func"  ${file/%.c/.o} 2> patches/$syscall.result
-	patch -f -p2 -R < patches/$syscall.patch
+#	patch -f -p2 -R < patches/$syscall.patch
+	git checkout -f master
 done
