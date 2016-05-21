b00c6550fcd500dd107d5fee7da34714e671561c
Laurent Georget <laurent.georget@supelec.fr>
2016-05-21 14:43:55 +0200

    Generalize the patches/*.zsh script

--- patches/extract.zsh
+++ patches/extract.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
-curdir=$(pwd)
-repo=/home/lgeorget/projects/kayrebt/kayrebt-path_examiner/linux
+curdir="$(pwd)"
+repo="$1"
 base_commit=bbb70443
 
 pushd $repo
@@ -9,7 +9,7 @@ while read -A line; do
 	syscall=${line[1]}
 	branch=${line[2]}
 	commit=${line[3]}
-	patch_file=$curdir/$syscall.patch
+	patch_file="$curdir/$syscall.patch"
 	git checkout $branch
 	git diff -p $base_commit $commit -- . > $patch_file
 done
